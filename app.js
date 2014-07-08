3f3fc8055aaf3666e1005ce0788837c8486dc5fe
Kevin Liang <kevin1liang@gmail.com>
2014-07-07 19:54:32 -0700

    Generates map based on user's location.

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
-// function get_location() {
-//   navigator.geolocation.getCurrentPosition(show_map);
-// };
-
-
-// function show_map(position) {
-//   var latitude = position.coords.latitude;
-//   var longitude = position.coords.longitude;
-//   console.log('success! latitude: ' + latitude + ', longitude: ' + longitude);
-// };
-
-var map = L.map('map').setView([37.783691499999996, -122.4091494], 13);
+var map = L.map('map').locate({setView: false, maxZoom: 15})/*;setView([34.783691499999996, -100.4091494], 15);*/
 
 L.tileLayer('http://{s}.tiles.mapbox.com/v3/kevin1liang.inaoeoj4/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18
 }).addTo(map);
 
-// get_location();
+$('.getLocation').on('click', function(){
+  console.log("CLICKED");
+  map.locate({setView: true, maxZoom: 15});
+});
+
+
+
 
 //MapID: kevin1liang.inaoeoj4
+ 
+// /** bind the locationfound event to the function onLocationFound()
+//     in other words, tell Leaflet what to do once locate() is successful 
+// **/
+// mymap.on('locationfound', onLocationFound);
+ 
+// function onLocationFound(e) {
+//     // create a marker at the users "latlng" and add it to the map
+//     L.marker(e.latlng).addTo(mymap);
+// }
