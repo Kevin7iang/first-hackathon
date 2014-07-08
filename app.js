453ce448fbdba7f3d62d660a2d4562b77f7ea241
Kevin Liang <kevin1liang@gmail.com>
2014-07-07 22:50:32 -0700

    Added marker functionality with title properties (which will be used for sorting)

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
-var map = L.map('map').locate({setView: false, maxZoom: 15})/*;setView([34.783691499999996, -100.4091494], 15);*/
-
+var map = L.map('map').locate({setView: false, maxZoom: 15})/*;setView([37.783688, -122.4091485], 15);*/
+var storage = [];
 L.tileLayer('http://{s}.tiles.mapbox.com/v3/kevin1liang.inaoeoj4/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18
 }).addTo(map);
 
 $('.getLocation').on('click', function(){
-  console.log("CLICKED");
   map.locate({setView: true, maxZoom: 15});
 });
 
+$('.setMarker').on('click', function(){
+  L.marker([37.783688, -122.4091485]).addTo(map).bindPopup('Hack Reactor');
+});
 
-
+map.on('click', function(e) {
+  var activity = prompt("Please enter activity type:");
+  var marker = L.marker([e.latlng.lat, e.latlng.lng], {title: activity});
+  marker.addTo(map);
+  storage.push(marker);
+  console.log(marker.options.title);
+  // L.marker.title = prompt('Enter an activity:');
+  console.log("Latitude: " + e.latlng.lat + ", Longitude: " + e.latlng.lng + ", Title: " + activity);
+  console.log(storage);
+});
 
 //MapID: kevin1liang.inaoeoj4
  
